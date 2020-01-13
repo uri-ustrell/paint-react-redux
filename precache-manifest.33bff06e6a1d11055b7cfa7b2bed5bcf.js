a174662285d911b6177b0261bdb7fb6df9de27f3
root <root@DESKTOP-QRLQ7Q8.localdomain>
2020-01-13 23:13:00 +0100

    Updates

---
+++ precache-manifest.33bff06e6a1d11055b7cfa7b2bed5bcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78757425db95759f8264d49c5173d332",
+    "url": "/paint-react-redux/index.html"
+  },
+  {
+    "revision": "39cfecdae26b93701786",
+    "url": "/paint-react-redux/static/css/main.c2e13e45.chunk.css"
+  },
+  {
+    "revision": "543042226080882ae2cd",
+    "url": "/paint-react-redux/static/js/2.332de2d5.chunk.js"
+  },
+  {
+    "revision": "2fbea69272bdf1f8216f8d564f436930",
+    "url": "/paint-react-redux/static/js/2.332de2d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39cfecdae26b93701786",
+    "url": "/paint-react-redux/static/js/main.12765685.chunk.js"
+  },
+  {
+    "revision": "283095dcf1939eac1456",
+    "url": "/paint-react-redux/static/js/runtime-main.5b7037f2.js"
+  }
+]);
